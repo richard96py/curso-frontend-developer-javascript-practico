43817e83b7843b9335741a3b24373c5505188524
richard96py <garcia.richard96@outlook.com>
2022-12-29 20:11:53 -0300

    Fusion del menu agregando el destkop-menu

---
+++ main.js
@@ -0,0 +1,8 @@
+const menuEmail = document.querySelector('.navbar-email');
+const destktopMenu = document.querySelector('.desktop-menu');
+
+menuEmail.addEventListener('click',toggleDesktopMenu);
+
+function toggleDesktopMenu(){
+    destktopMenu.classList.toggle('inactive');
+}
