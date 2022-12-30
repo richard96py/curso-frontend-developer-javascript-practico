e733f219d31ae81bf609ce640c9937f3c966e105
richard96py <garcia.richard96@outlook.com>
2022-12-30 16:56:38 -0300

    Fusion de mobile menu

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
 const menuEmail = document.querySelector('.navbar-email');
 const destktopMenu = document.querySelector('.desktop-menu');
+const iconHamMenu = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 menuEmail.addEventListener('click',toggleDesktopMenu);
+iconHamMenu.addEventListener('click', toggleMobileMenu);
+
+function toggleMobileMenu(){
+    mobileMenu.classList.toggle('inactive');
+}
 
 function toggleDesktopMenu(){
     destktopMenu.classList.toggle('inactive');
